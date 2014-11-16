eddddf71573ff5999dc0e9e91b8d1f0e7639e8e9
openlabocean <tobi@openlab.org.uk>
2014-11-16 01:07:20 +0000

    4th gui search render

--- bizbean_cli.py
+++ bizbean_cli.py
@@ -9,22 +9,31 @@ app = Flask(__name__)
 #get rid of this soon
 @app.route('/')
 def main_page():
-    r = get("http://api.bizbean.co.uk/London/Accountants")
-    query_response = loads(r.content)
-    lead = dict(query_response[3])
+    return render_template('search.html')
 
-    return render_template('index.html', 
-        business_name=lead["BusinessName"], 
-        addr1=lead["Addr1"], 
-        addr2=lead["Addr2"],  
-        phone_number=lead["Phone"],
-        post_code=lead["PostCode"],
-        city=lead["City"],
-
-         )      #  county=lead["County"],
+@app.route('/results')
+def results():
+    return render_template('business-list.html')
 
 
 
+
+
+# @app.route('/')
+# def main_page():
+#     r = get("http://api.bizbean.co.uk/London/Accountants")
+#     query_response = loads(r.content)
+#     lead = dict(query_response[3])
+
+#     return render_template('search.html', 
+#         business_name=lead["BusinessName"], 
+#         addr1=lead["Addr1"], 
+#         addr2=lead["Addr2"],  
+#         phone_number=lead["Phone"],
+#         post_code=lead["PostCode"],
+#         city=lead["City"],
+
+#          )      #  county=lead["County"],
 
 
 
